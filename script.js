53790f32d3c5dc4add8f8f285e61c420237c291d
kay <00000f22222@gmail.com>
2025-01-26 21:22:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent page reload
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    
-    alert(`Hi, ${name}! I've received your message. I'll get back to you @ ${email} soon.`);
-});
